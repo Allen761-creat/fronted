f6298ea3e210044a2d297fb0d412e4d36022a337
Allen761-creat <allenanwer69@gmail.com>
2025-01-26 02:53:58 -0800

    Your commit message

--- src/Config/Apis.jsx
+++ src/Config/Apis.jsx
@@ -1,8 +1,8 @@
  const apis=[
-    "http://localhost:8080/api/v1/users",
+    "https://backend-five-nu-17.vercel.app/api/v1/users",
     "https://backend-five-nu-17.vercel.app/api/v1/products",
-    "http://localhost:8080/api/v1/admin",
-   "http://localhost:8080/api/v1/orders",
+    "https://backend-five-nu-17.vercel.app/api/v1/admin",
+   "https://backend-five-nu-17.vercel.app/api/v1/orders",
 
    
  ]
